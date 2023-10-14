8331f18d1db8a9bb36a9967653800544dd69ca33
quanvannfq <quan.van@nfq.com>
2023-10-14 17:19:22 +0700

    add lombok

--- src/main/java/quanvan/todoapp/entity/Todo.java
+++ src/main/java/quanvan/todoapp/entity/Todo.java
@@ -4,47 +4,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Data;
 
 @Entity
+@Data
 public class Todo {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String content;
 	private Boolean completed = Boolean.FALSE;
-	
-	public Todo(Long id, String content, Boolean completed) {
-		super();
-		this.id = id;
-		this.content = content;
-		this.completed = completed;
-	}
-
-	public Todo() {
-
-	}
-
-	public Long getId() {
-		return id;
-	}
-	
-	public void setId(Long id) {
-		this.id = id;
-	}
-	
-	public String getContent() {
-		return content;
-	}
-	
-	public void setContent(String content) {
-		this.content = content;
-	}
-	
-	public Boolean getCompleted() {
-		return completed;
-	}
-	
-	public void setCompleted(Boolean completed) {
-		this.completed = completed;
-	}
 }
